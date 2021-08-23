5f6c9099df8bbbfb2930e5345a19220ae164c9a8
Jen Beckett <jennybeckett2@hotmail.com>
2021-08-23 15:58:07 -0700

    Lots of logic Completed for Safety
    Cards Hide as you go now for Power + Safety

--- script.js
+++ script.js
@@ -2,21 +2,9 @@ $(document).ready(function (){
     $('.quizContainer').hide(); //hide all cards, then below, show only the starting card
     $('#0').show();
 
-    // $('#createCase').show(); Case or Continue monitoring cards.
-    // $('#everythingOkay').show();
-
-
-    // $('#submitQ1').click( () => { //home landing page
-    //     let category =  $('input[name="problemCategory"]:checked').val();
-    //     while (category == undefined)
-    //         $('#0').append('<div class="errorText">Please select an option to continue</div>');
-    //
-    //
-    //
-    // }); // end click function q0
 
     $('#submit7').click( () => {
-        $('#camCheck').hide();
+        $('#camCheck').hide(); //take this out when done
     });
 
         $('#submitQ1').click( () => { //home landing page
@@ -26,7 +14,7 @@ $(document).ready(function (){
             // $('#camCheck').show();  un comment out later when finish
 
         switch (category) {
-            case "power": //incomplete
+            case "power": //incomplete - need gen cards
                 $('#1_1').show();
                 break;
             case "safety": //incomplete
@@ -46,7 +34,7 @@ $(document).ready(function (){
 
         }// end switch q0
         if (category != undefined) {
-            // $('#0').hide();
+             $('#0').hide();
         }
     }); // end click function q0
 
@@ -83,7 +71,7 @@ $(document).ready(function (){
             //category = 0;
         }// end switch q0
         if (powerCat != undefined) {
-            //$('#1_1').hide();
+            $('#1_1').hide();
         }
 
     }); //END POWER CARD  landing page
@@ -94,22 +82,24 @@ $(document).ready(function (){
                 let  wintT3=  $('#windT3').val();
                 let  wintT4=  $('#windT4').val();
 
+                if (wintT3 === "") //does not let wind speed be blank
+                    $('#1_A').prepend(`<span> Please Complete all fields.</span>`);
+                else {
+                    if (wintT1 && wintT2 === "Yes") //data from other WTG avail +  is producing power
+                        $('#createCase').show();
 
-                if (wintT1 && wintT2 === "Yes") //data from other WTG avail +  is producing power
-                    $('#createCase').show();
+                    else if (wintT4 === "No") //wtg not moving in same direction
+                        $('#createCase').show();
 
-                else if (wintT4 === "No") //wtg not moving in same direction
-                    $('#createCase').show();
+                    else if ((wintT1 === "No") && (wintT3 > 4))//no data for other wtg but winds are strong
+                        $('#createCase').show();
 
-                else if ((wintT1 === "No" ) && (wintT3 > 4) )//no data for other wtg but winds are strong
-                    $('#createCase').show();
+                    else
+                        $('#everythingOkay').show();
 
-                else
-                    $('#everythingOkay').show();
-
+                    $('#1_A').hide();
+                }
             }); //END wind turbine page
-
-
 
             $('#submit1_B').click( () => { //solar power card: determines if case needed
                 let solarP1 = $('#solarP1').val();
@@ -130,9 +120,8 @@ $(document).ready(function (){
                 else
                     $('#everythingOkay').show();
 
-
+                $('#1_B').hide();
             }); // END solar power page
-
 
 
             $('#submit1_C').click( () => { //Gen Fuel Cell card: determines if case needed
@@ -145,8 +134,6 @@ $(document).ready(function (){
             });
 
 
-
-
             $('#submit1_D').click( () => { //Gen Diesel card: determines if case needed
 
 
@@ -155,6 +142,110 @@ $(document).ready(function (){
 
 
             });
+
+
+    $('#submit2_1').click( () => { //safety card landing page
+        let safeCat =  $('input[name="safeCat"]:checked').val();
+        console.log(safeCat);
+
+        switch (safeCat) {
+            case "1": //Water Level Sensor
+                $('#2_A').show();
+                console.log("i'm in case one");
+                break;
+            case "2":// Hatch Intrusion
+                $('#2_B').show();
+                break;
+            case "3": //OffPos
+                $('#2_C').show();
+                break;
+            default:
+                $('#2_1').append('<div class="errorText">Please select an option to continue</div>');
+            //category = 0;
+        }// end switch safety landing
+        if (safeCat != undefined) {
+            $('#2_1').hide();
+        }
+    }); //end safety landing card
+
+
+            $('#submit2_A').click( () => { //WLS LOGIC CARD
+                let value1 = $('#WLS1').val();
+                let value2 = $('#WLS2').val();
+                let value3 = $('#WLS3').val(); //which compartment will be used for adding info into smartsheet.
+
+                if (value1 < value2) //it's not sunny or night
+                    $('#createCase').show();
+                else
+                    $('#everythingOkay').show();
+
+                $('#2_A').hide();
+            }); // END WLS LOGIC CARD
+
+
+            $('#submit2_B').click( () => { //Hatch Card
+                let value1 = $('#checkHatch').val();
+
+                if (value1 === "Yes") //it's not sunny or night
+                    $('#createCase').show();
+                else
+                    $('#everythingOkay').show();
+
+                $('#2_B').hide();
+            }); // END Hatch LOGIC CARD
+
+            $('#submit2_C').click( () => { //Off Pos Logic card
+                let value1 = $('#offPos1').val();
+                let value2 = $('#offPos2').val();
+
+
+                if (value1 && value2 === "Yes" ) //buoy might be floating away
+                    $('#2_C_2').show();
+                else
+                    $('#2_C_3').show(); //check DMS coordinates
+
+                $('#2_C').hide();
+            }); // END Off Pos Logic card
+
+                    $('#submit2_C_2').click( () => { //Off Alerts checking severity pt1
+                        let value1 = $('#oPos1').val();
+                        let value2 = $('#oPos2').val();
+
+                        console.log(value1 + " is val1 2c2");
+                        console.log(value2+ " is val2");
+
+                        if (value1 === "Yes" && value2 === "Yes" ) { //buoy might be floating away
+                            $('#createCase').show();
+                            $('#createCase').append(`<span> CALL MAX </span> <br>`);
+                        }
+
+                        else
+                            $('#2_C_3').show(); //check DMS coordinates
+
+                        $('#2_C_2').hide();
+                    }); // END Off Pos Logic card
+
+
+                    $('#submit2_C_3').click( () => { //checking DMS settings
+                        let value1 = $('#oPos3').val();
+                        let value2 = $('#oPos4').val();
+
+                        if (value1 === "Yes" && value2 === "No" ) { //buoy had the incorrect watchCircle set.
+                            $('#everythingOkay').show();
+                            $('#everythingOkay').prepend(`It appears that the Watch Circle had the incorrect coordinates set. <br>
+                                Continue to monitor the off-position messages to ensure the new coordinate message takes. `);
+                        }
+
+                        else
+                        { // check comms? //
+                             }
+
+                        $('#submit2_C_3').hide();
+                    }); // END Off Pos Logic card
+
+
+
+
 
 
 });//end document on ready
